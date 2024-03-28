dba484b85226a3d4c323d8b8d1a49e40cc1c95e9
PoojanShahN01609604 <163201620+PoojanShahN01609604@users.noreply.github.com>
2024-03-28 10:15:33 -0400

    linux module

--- main.tf
+++ main.tf
@@ -1,28 +1,66 @@
-module "resource_group" {
-  source = "/home/uday/automation/terraform/lab06/modules/resource_group"
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "${var.linux_name}-nic-${format("%1d", count.index + 1)}"
+  location            = var.linux_Rg_location
+  resource_group_name = var.linux_Rg_name
+  tags                = local.common_tags
+  count               = var.nb_count
 
-  network-Rg = "network-Rg"
-  linux-Rg   = "linux-Rg"
-  windows-Rg = "windows-Rg"
-  location   = "UK West"
+  ip_configuration {
+    name                          = "${var.linux_name}-ipconfig-${format("%1d", count.index + 1)}"
+    subnet_id                     = var.subnet1_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.linux-pip[*].id, count.index + 1)
+
+  }
 }
 
-module "network" {
-  source = "/home/uday/automation/terraform/lab06/modules/network"
-  network_Rg_location  = module.resource_group.network_Rg_location
-  network_Rg_name = module.resource_group.network_Rg_name
+resource "azurerm_public_ip" "linux-pip" {
+  name                = "${var.linux_name}-pip-${format("%1d", count.index + 1)}"
+  location            = var.linux_Rg_location
+  tags                = local.common_tags
+  resource_group_name = var.linux_Rg_name
+  domain_name_label   = "${var.linux_name}-${format("%1d", count.index + 1)}"
+  allocation_method   = "Dynamic"
+  count               = var.nb_count
 }
 
-module "linux" {
-  source = "/home/uday/automation/terraform/lab06/modules/linux"
-  linux_Rg_location  = module.resource_group.linux_Rg_location
-  linux_Rg_name = module.resource_group.linux_Rg_name
-  subnet1_id = module.network.subnet1_id
+resource "azurerm_linux_virtual_machine" "vmlinux" {
+  name                  = "${var.linux_name}-${format("%1d", count.index + 1)}"
+  count                 = var.nb_count
+  location              = var.linux_Rg_location
+  tags                  = local.common_tags
+  resource_group_name   = var.linux_Rg_name
+  network_interface_ids = [element(azurerm_network_interface.linux-nic[*].id, count.index + 1)]
+  computer_name         = "${var.linux_name}-${format("%1d", count.index + 1)}"
+  size                  = var.linux_size
+  admin_username        = var.admin_username
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key)
+  }
+
+  os_disk {
+    name                 = "${var.linux_name}-os-disk-${format("%1d", count.index + 1)}"
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_account_type
+    disk_size_gb         = var.os_disk_size
+  }
+
+  source_image_reference {
+    publisher = var.ubuntu_publisher
+    offer     = var.ubuntu_offer
+    sku       = var.ubuntu_sku
+    version   = var.ubuntu_version
+  }
 }
 
-module "windows" {
-  source = "/home/uday/automation/terraform/lab06/modules/windows"
-  windows_Rg_location  = module.resource_group.windows_Rg_location
-  windows_Rg_name = module.resource_group.windows_Rg_name
-  subnet2_id = module.network.subnet2_id
+resource "azurerm_availability_set" "linux_avs" {
+  name                         = var.linux_avs
+  resource_group_name          = var.linux_Rg_name
+  location                     = var.linux_Rg_location
+  managed                      = true
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 5
 }
+
